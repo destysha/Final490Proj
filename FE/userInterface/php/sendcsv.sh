84f435665286a5be0a5025f77ba5b99f1e70a127
Shaiddy Perez <sp2235@njit.edu>
2019-05-02 19:02:27 -0400

    Updating CSV upload files - Fixing permissions to be updated

--- FE/userInterface/php/sendcsv.sh
+++ FE/userInterface/php/sendcsv.sh
@@ -13,15 +13,20 @@ FILE=`ls`
 # password for root on BE vm
 pAs="passwd"
 
+echo  "deleteing old csv from BE...\n"
 # Go to BE : ~/Upload and delete any old csv files
 /usr/bin/sshpass -p ${pAs} ssh -t root@"${IP_D}" "cd ~/Upload/File/; sudo rm -r ./*.csv;"
+
+echo  "Sending new csv to BE...\n"
 
 # Sending new csv file to BE
 /usr/bin/sshpass -p ${PASS_M} scp ${FILE} "${User_M}"@"10.0.2.11":~/Upload/File/
 
+echo  "deleteing old csv from FE...\n"
+
 # Delete new csv file from FE /var/www/ishop/userInterface/uploads/
 cd /var/www/ishop/userInterface/uploads/
-sudo rm -r ./*.csv
+`sudo rm -r ./*.csv`
 
 # On BE: Upload csv file to DB 
 /usr/bin/sshpass -p ${pAs} ssh -t root@"${IP_D}" "cd ~/Upload/; exec(./uploadcsv.php);"
